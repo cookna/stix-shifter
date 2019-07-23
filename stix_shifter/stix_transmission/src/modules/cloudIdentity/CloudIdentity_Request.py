a412a96e784e100280d8e0aa862c8f60900b5661
NathanC <Nathan.A.Cook@ibm.com>
2019-07-23 11:58:44 -0400

    Updated Request

--- stix_shifter/stix_transmission/src/modules/cloudIdentity/CloudIdentity_Request.py
+++ stix_shifter/stix_transmission/src/modules/cloudIdentity/CloudIdentity_Request.py
@@ -60,6 +60,64 @@ def postReports(token, reportName, FROM, TO, SIZE, SORT_BY, SORT_ORDER, SEARCH_A
         print("failed")
         print(e)
 
+def getAdminActivity(token, FROM, TO, SIZE, SORT_BY, SORT_ORDER):
+    url = util.getUri() + "/v1.0/reports/admin_activity"
+    headers = { "Accept": "application/json, text/plain, */*","Content-Type": "application/json", "authorization": "Bearer "+token}
+    body = json.dumps({
+        "FROM": FROM,
+        "TO": TO,
+        "SIZE": SIZE,
+        "SORT_BY": SORT_BY,
+        "SORT_ORDER": SORT_ORDER
+    })
+
+    try:
+        res = requests.post(url, data=body, headers=headers)
+
+        jsonData = res.json()
+        print(jsonData)
+    except Exception as e:
+        print("failed")
+        print(e)
+
+def getAuthenticatedTotalLogins(token, FROM, TO):
+    url = util.getUri() + "/v1.0/reports/auth_total_logins"
+    headers = { "Accept": "application/json, text/plain, */*","Content-Type": "application/json", "authorization": "Bearer "+token}
+    body = json.dumps({
+        "FROM": FROM,
+        "TO": TO,
+    })
+
+    try:
+        res = requests.post(url, data=body, headers=headers)
+
+        jsonData = res.json()
+        print(jsonData)
+    except Exception as e:
+        print("failed")
+        print(e)
+
+def getAuthenticationTrail(token, FROM, TO, SIZE, SORT_BY, SORT_ORDER):
+    url = util.getUri() + "/v1.0/reports/auth_audit_trail"
+    headers = { "Accept": "application/json, text/plain, */*","Content-Type": "application/json", "authorization": "Bearer "+token}
+    body = json.dumps({
+        "FROM": FROM,
+        "TO": TO,
+        "SIZE": SIZE,
+        "SORT_BY": SORT_BY,
+        "SORT_ORDER": SORT_ORDER
+    })
+
+    try:
+        res = requests.post(url, data=body, headers=headers)
+
+        jsonData = res.json()
+        print(jsonData)
+    except Exception as e:
+        print("failed")
+        print(e)
+
+
 def getAuthFactors(token, params=None):
     url = util.getUri() + f"/v1.0/authnmethods"
 
